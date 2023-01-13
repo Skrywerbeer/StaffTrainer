dae258864b999bdbea432bed81701f61e3f82289
Skrywerbeer <liquidsquid1@gmail.com>
2023-01-14 01:32:55 +0200

    Added basic sound.

--- main.ts
+++ main.ts
@@ -37,6 +37,7 @@ function* TrebleNoteGen(n: number = -1) {
 	// ];
 const PITCH_CLASSES = ["A", "B", "C", "D", "E", "F", "G"];
 
+
 const KEYBOARD = document.getElementById("keyboard");
 function initKeyboard() {
 		//	KEYBOARD.setAttribute("width", String(SVG_WIDTH));
@@ -56,18 +57,51 @@ const NUMBER_OF_NOTES = 4;
 let notes = [];
 let noteIndex = 0;
 
+function* FrequencyLUT() {
+	const NUMBER_OF_OCTAVES = 6;
+	const FIRST_OCTAVE = 1;
+	// const LUT = [];
+	const INTERVALS = [0, 2, 4, 5, 7, 9, 10];
+	const C0_FREQ = 16.35159*4;// Transpose up two octaves
+	for (let i = FIRST_OCTAVE; i < NUMBER_OF_OCTAVES + FIRST_OCTAVE; ++i) {
+		const notes = [...noteRange(i)]
+		for (let j = 0; j < notes.length; ++j)
+			// LUT.push({note: notes[j],
+			// 		  frequency: C1_FREQ*Math.pow(2, 12*i + INTERVALS[j])});
+			yield {note: notes[j],
+				   frequency: C0_FREQ*Math.pow(2, (12*i + INTERVALS[j])/12)};
+	}
+}
+const LUT = [...FrequencyLUT()];
+
 function newGame() {
 	notes = [...TrebleNoteGen(NUMBER_OF_NOTES)];
 	console.log(`New notes: ${notes}`);
 	noteIndex = 0;
 	stave.addNotes(notes);
 }
-
+const audioCtx = new AudioContext();
 function clickHandler(event) {
 	const letter = event.target.textContent;
 	if (letter === notes[noteIndex].charAt(0)) {
 		const marker = stave.markerGroups.shift();
 		marker.remove();
+		
+		const note = LUT.find((element) => {
+			if (element.note === notes[noteIndex])
+				return true;
+		});
+		if (note === undefined)
+			console.log(":O panic");
+		if (audioCtx.state === "suspended")
+			audioCtx.resume();
+		const osc = new OscillatorNode(audioCtx, {
+			type: "sine",
+			frequency: note.frequency,
+		});
+		osc.connect(audioCtx.destination);
+		osc.start(audioCtx.currentTime);
+		osc.stop(audioCtx.currentTime + 0.2);
 		noteIndex++;
 	}
 	
